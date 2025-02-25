ed28bd0962100a57e669f9b569ebd1e3f5590ac3
Shane-Coffey-3 <shanejcoffey27@gmail.com>
2025-02-25 09:04:44 -0600

    Update BoardSpace.java
    rook movement

--- src/BoardSpace.java
+++ src/BoardSpace.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class BoardSpace extends JPanel {
     public static final Color LIGHT_BROWN = new Color(154, 123, 79);
     public static final Color DARK_BROWN = new Color(75, 37, 28);
     
     private static BoardSpace selectedSpace = null;
+    private static ArrayList<BoardSpace> highlightedSpaces = new ArrayList<>();
     
     private final int[] coordinate;
     private Piece piece;
@@ -21,28 +23,53 @@ public class BoardSpace extends JPanel {
         
         setPreferredSize(new Dimension(60, 60));
         
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                highlight();
-            }
-        });
+        
     }
     
-    private void highlight() {
-    if (selectedSpace == this) {
-        setColor(originalColor);
-        selectedSpace = null;
-    } else {
-        if (selectedSpace != null) {
+    public void highlight() {
+        if (selectedSpace == this) {
+            clearHighlightedMoves();
+            setColor(originalColor);
+            selectedSpace = null;
+        } else {
+            if(selectedSpace != null) {
+                selectedSpace.setColor(selectedSpace.originalColor);
+            }
+            
+            setColor(Color.GREEN);
+            selectedSpace = this;
+        }
+    }
+    
+    public void highlightMoves(int[][] moves, BoardSpace[][] board) {
+        clearHighlightedMoves();
+        
+        for(int[] move : moves) {
+            int y = move[0];
+            int x = move[1];
+            if(y >= 0 && y < 8 && x >= 0 && x < 8) {
+                board[y][x].setColor(Color.YELLOW);
+                highlightedSpaces.add(board[y][x]);
+            }
+        }
+    }
+    
+    public static void clearHighlightedMoves() {
+        for(BoardSpace space : highlightedSpaces) {
+            if(space.getColor() == Color.YELLOW) {
+                space.setColor(space.originalColor);
+            }
+        }
+        highlightedSpaces.clear();
+    }
+    
+    public static void clearAllHighlights() {
+        clearHighlightedMoves();
+        if(selectedSpace != null) {
             selectedSpace.setColor(selectedSpace.originalColor);
         }
-        
-        setColor(Color.YELLOW);
-        selectedSpace = this;
     }
-}
-
+    
     
     public void setPiece(Piece piece) {
         this.piece = piece;
@@ -66,13 +93,17 @@ public class BoardSpace extends JPanel {
         return color;
     }
     
+    public static BoardSpace getSelectedSpace() {
+        return selectedSpace;
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(color);
         g.fillRect(0, 0, getWidth(), getHeight());
         
-        if (piece != null) {
+        if(piece != null) {
             piece.draw(g, getWidth() / 2, getHeight() / 2, getWidth());
         }
     }
